c5d151d9b739d7bfcde4783c8ee0161ce335a4df
Guitasy <wang_lianyi@outlook.com>
2020-03-09 20:42:18 +0800

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reverse reverse = new Reverse();
+            reverse.Write("Welcome to JoyCastle. Let's make an awesome game together. ");
+            reverse.Write("Focused, hard work is the real key to success. Keep your eyes on the goal, and just keep taking the next step towards completing it. ");
+            Console.ReadKey();
+        }
+    }
+
+    class Reverse
+    {
+        StringBuilder _whole, _sentence;
+
+        public Reverse()
+        {
+            _whole = new StringBuilder();
+            _sentence = new StringBuilder();
+        }
+
+        public void Write(string str)
+        {
+            _whole.Clear();
+            _sentence.Clear();
+            int insertIndex = 0;
+            bool isStartWithSpace = false;
+            for (int i = 0, max = str.Length; i < max; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    if (_sentence.Length == 0)
+                    {
+                        isStartWithSpace = true;
+                        continue;
+                    }
+                    _sentence.Insert(0, ' ');
+                    insertIndex = 0;
+                    continue;
+                }
+                if (str[i] == '.' || str[i] == ',')
+                {
+                    _sentence.Append(str[i]);
+                    if (isStartWithSpace)
+                    {
+                        _sentence.Insert(0, ' ');
+                    }
+                    _whole.Append(_sentence);
+                    _sentence.Clear();
+                    insertIndex = 0;
+                    isStartWithSpace = false;
+                }
+                else
+                {
+                    _sentence.Insert(insertIndex, str[i]);
+                    insertIndex++;
+                }
+            }
+            Console.WriteLine(_whole.ToString());
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new SquareFill().Calculate();
+        }
+    }
+
+    class SquareFill
+    {
+        int _m, _n;
+        float _area;
+        const int countSquare = 100;
+
+        public void Calculate()
+        {
+            _m = ReadInt();
+            _n = ReadInt();
+            _area = _m * _n;
+
+            float lMax = (float)Math.Sqrt(_area / countSquare);
+            float blankArea = float.MaxValue, length = 0;
+            for (int i = (int)Math.Floor(_m / lMax); i <= 100; i++)
+            {
+                float area = CalculateBlankArea(i, out float l);
+                if (area < blankArea)
+                {
+                    blankArea = area;
+                    length = l;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(string.Concat("填充正方形边长为：", length));
+            Console.ReadKey();
+        }
+
+        int ReadInt()
+        {
+            string str = string.Empty;
+            while (true)
+            {
+                Console.Write("请输入边长数字：");
+                str = Console.ReadLine();
+                if (int.TryParse(str, out int num))
+                {
+                    if (num > 0)
+                    {
+                        return num;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 通过m边填充正方形数量计算空白区域面积
+        /// </summary>
+        /// <param name="countM">m边填充方块数</param>
+        /// <param name="length">正方形边长</param>
+        /// <returns>空白区域面积</returns>
+        float CalculateBlankArea(int countM, out float length)
+        {
+            if (countM == 0)
+            {
+                length = 0;
+                return _area;
+            }
+            int countN = (int)Math.Ceiling(countSquare / (float)countM);
+            length = Math.Min(_m / (float)countM, _n / (float)countN);
+            return _area - length * length * countSquare;
+        }
+    }
+}
